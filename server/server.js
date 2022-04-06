61251d60952d6eee1239aa6638694f68aaf76063
J.P. Doyle <jp.hackreactor@gmail.com>
2022-04-05 19:16:53 -0700

    Initialized server and defined routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,42 @@
 const express = require('express');
-const db = require('../database/database');
 
-const app = express();
+const api = express();
+api.use(express.urlencoded());
 const port = 3000;
 
-app.listen(port, () => {
+api.get('/reviews/', (req, res) => {
+  // product_id value NOT trusted
+  const productId = req.query.product_id;
+  const page = req.body.page;
+  const count = req.body.count;
+  const sort = req.body.sort;
+  //
+});
+
+api.get('/reviews/meta/', (req, res) => {
+  // product_id value NOT trusted
+  const productId = req.query.product_id;
+  //
+});
+
+api.post('/reviews/', (req, res) => {
+  // req.body values NOT trusted
+  const productId = req.body.product_id;
+  const { rating, summary, body, recommend, name, email, photos, characteristics } = req.body;
+  //
+});
+
+api.put('reviews/:review_id/helpful', (req, res) => {
+  //
+  const reviewId = req.params.review_id;
+});
+
+api.put('reviews/:review_id/report', (req, res) => {
+  //
+  const reviewId = req.params.review_id;
+});
+
+
+api.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
