fc44c79a6f0378ca89d5d6c7d9d5484322848fe7
J.P. Doyle <jp.hackreactor@gmail.com>
2022-04-09 11:38:19 -0700

    Further updates to db helper functions

--- database/database.js
+++ database/database.js
@@ -5,9 +5,6 @@ const client = new MongoClient(url);
 const dbName = 'reviews_test';
 
 let db;
-let sortBy;
-let nextReviewId;
-
 
 const connect = async () => {
   try {
@@ -17,35 +14,6 @@ const connect = async () => {
     module.exports = db;
   } catch (error) {
     console.log('Error connecting to database');
-  }
-};
-//
-const getReviews = async (productId, sort) => {
-  //
-  if (sort === 'relevant') {
-    sortBy = { helpfulness: -1, date: -1 };
-  } else if (sort === 'newest') {
-    sortBy = { date: -1 };
-  } else {
-    sortBy = { helpfulness: -1 };
-  }
-  //
-  try {
-    const reviews = await db.reviews.aggregate([
-      {
-        $match: { product_id: productId },
-      }, {
-        $sort: sortBy,
-      }, {
-        $limit: 1000,
-      },
-    ], {
-      allowDiskUse: true,
-    });
-    return reviews;
-  } catch (error) {
-    console.log('Error retrieving reviews', error);
-    return 0;
   }
 };
 
@@ -59,92 +27,35 @@ const getProductMetadata = async (productId) => {
   }
 };
 
-const addReview = async (review) => {
-  try {
-    await db.collection('reviews').insertOne({
-      product_id: review.product_id,
-      rating: review.rating,
-      date: { '$date': new Date().toISOString() },
-      summary: review.summary,
-      body: review.body,
-      recommend: review.recommend,
-      reported: false,
-      reviewer_name: review.name,
-      reviewer_email: review.email,
-      response: null,
-      helpfulness: 0,
-      photos: review.photos,
-      review_id: nextReviewId,
-      characteristics: review.characteristics
-    });
-    nextReviewId += 1;
-  } catch (error) {
-    console.log('Erroring adding new review', error);
-    return 0;
+const getReviews = async (productId, sort) => {
+  let sortBy;
+  //
+  if (sort === 'relevant') {
+    sortBy = { helpfulness: -1, date: -1 };
+  } else if (sort === 'newest') {
+    sortBy = { date: -1 };
+  } else {
+    sortBy = { helpfulness: -1 };
   }
-  // Recalculate metadata
-  let recommend = review.recommend ? true : false;
   //
   try {
-    let reviewCount = await db.reviews.aggregate(
-      [
-        { $match: { product_id: review.product_id } },
-        { $count: 'count' }
-      ], {
-        allowDiskUse: true
-      }
-    );
-    await db.collection('product_metadata').updateOne(
+    const reviews = await db.reviews.aggregate([
       {
-        product_id: review.product_id
-      }, [
-        { $set: { `ratings.${review.rating}`: { $add: [1, `$ratings.${review.rating}`] } } },
-        { $set: { `${recommend}`: { $add: [1, `$recommend.${recommend}`] } } },
-        { $set: {
-          characteristics: {
-            $expr: {
-              $arrayToObject: {
-                $map: {
-                  input: {
-                    $objectToArray: '$characteristics'
-                  },
-                  in: {
-                    $zip: [
-                      [ '$$this.0' ],
-                      [ {
-                        id: "$$this.1.id",
-                        value: {
-                          $toString: {
-                            $divide: [
-                              { $sum: [
-                                { $multiply: [
-                                  reviewCount,
-                                  { $toInt: {
-                                    $toDecimal: '$$this.value'
-                                  } }
-                                ] },
-                                review.characteristics['$$this.1.id']
-                              ] },
-                              { $sum: [
-                                reviewCount,
-                                1
-                              ] }
-                            ]
-                          }
-                        }
-                      } ]
-                    ]
-                  }
-                }
-              }
-            }
-          }
-        } }
-     ]
-    );
-    return 1;
+        $match: {
+          product_id: productId,
+          reported: false,
+        },
+      }, {
+        $sort: sortBy,
+      }, {
+        $limit: 1000,
+      },
+    ], {
+      allowDiskUse: true,
+    });
+    return reviews;
   } catch (error) {
-    console.log('Error updating product metadata', error);
+    console.log('Error retrieving reviews', error);
     return 0;
   }
 };
@@ -174,3 +85,111 @@ const markReviewReported = async (reviewId) => {
     return 0;
   }
 };
+
+const postReview = async (review) => {
+  //
+  let nextReviewId; // also equal to number of reviews
+  //
+  try {
+    const result = await db.reviews.aggregate([
+      { $count: 'count' },
+    ], {
+      allowDiskUse: true,
+    });
+    nextReviewId = result.count + 1;
+  } catch (error) {
+    console.log('Error counting number of reviews', error);
+  }
+  //
+  try {
+    await db.collection('reviews').insertOne({
+      product_id: review.product_id,
+      rating: review.rating,
+      date: { $date: new Date().toISOString() },
+      summary: review.summary,
+      body: review.body,
+      recommend: review.recommend,
+      reported: false,
+      reviewer_name: review.name,
+      reviewer_email: review.email,
+      response: null,
+      helpfulness: 0,
+      photos: review.photos.map((uri) => ({ url: uri })),
+      review_id: nextReviewId,
+      characteristics: review.characteristics,
+    });
+  } catch (error) {
+    console.log('Erroring adding new review', error);
+    return 0;
+  }
+  //
+  try {
+    await db.collection('product_metadata').updateOne({
+      product_id: review.product_id,
+    }, [
+      { $set: { [`ratings.${review.rating}`]: { $add: [1, `$ratings.${review.rating}`] } } },
+      { $set: { [`${review.recommend}`]: { $add: [1, `$recommend.${review.recommend}`] } } },
+      {
+        $set: {
+          characteristics: {
+            $expr: {
+              $arrayToObject: {
+                $map: {
+                  input: {
+                    $objectToArray: '$characteristics',
+                  },
+                  in: {
+                    $zip: [
+                      ['$$this.0'],
+                      [{
+                        id: '$$this.1.id',
+                        value: {
+                          $toString: {
+                            $divide: [
+                              {
+                                $sum: [
+                                  {
+                                    $multiply: [
+                                      nextReviewId - 1,
+                                      {
+                                        $toInt: {
+                                          $toDecimal: '$$this.value',
+                                        },
+                                      },
+                                    ],
+                                  },
+                                  review.characteristics['$$this.1.id'],
+                                ],
+                              },
+                              {
+                                $sum: [
+                                  nextReviewId,
+                                  1,
+                                ],
+                              },
+                            ],
+                          },
+                        },
+                      }],
+                    ],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ]);
+    return 1;
+  } catch (error) {
+    console.log('Error updating product metadata', error);
+    return 0;
+  }
+};
+
+module.exports.db = db;
+module.exports.getProductMetadata = getProductMetadata;
+module.exports.getReviews = getReviews;
+module.exports.markReviewHelpful = markReviewHelpful;
+module.exports.markReviewReported = markReviewReported;
+module.exports.postReview = postReview;
